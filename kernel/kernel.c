015d75b2a12faa2e07bc951c2425da0139f2957c
Berat Beşli <beratbesli26@gmail.com>
2026-08-05 18:30:37 +0300

    Add BeerOS boot banner

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -6,6 +6,13 @@
 #include "shell.h"
 #include "vga.h"
 
+static const char boot_banner[] =
+    "BBBBBB   EEEEEEE  EEEEEEE  RRRRRR    OOOOO   SSSSSS\n"
+    "BB   BB  EE       EE       RR   RR  OO   OO SS\n"
+    "BBBBBB   EEEEE    EEEEE    RRRRRR   OO   OO  SSSSS\n"
+    "BB   BB  EE       EE       RR  RR   OO   OO      SS\n"
+    "BBBBBB   EEEEEEE  EEEEEEE RR   RR   OOOOO  SSSSSS\n\n";
+
 static void scheduler_task_one(void)
 {
     vga_write("BeerOS: scheduler gorev 1 calisti.\n");
@@ -19,6 +26,7 @@ static void scheduler_task_two(void)
 void kernel_main(void)
 {
     vga_clear();
+    vga_write(boot_banner);
     vga_write("BeerOS: protected mode ve VGA driver hazir.\n");
 
     pmm_init();
