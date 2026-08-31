63a3445abf71f0899e8e3b2b3bc8f0ca7441a31f
Berat Beşli <beratbesli26@gmail.com>
2026-08-31 21:36:16 +0300

    storage: probe FAT16 partitions from MBR

--- fs/vfs.c
+++ fs/vfs.c
@@ -4,11 +4,39 @@
 
 static struct fat_volume volume;
 
+static fat_u32_t read_u32(const fat_u8_t *data, unsigned int offset)
+{
+    return (fat_u32_t)data[offset] | ((fat_u32_t)data[offset + 1U] << 8U) |
+        ((fat_u32_t)data[offset + 2U] << 16U) | ((fat_u32_t)data[offset + 3U] << 24U);
+}
+
 void vfs_init(void)
 {
     volume.mounted = 0;
     if (ata_present()) {
-        fat_mount(&volume, ata_read_sectors, 0);
+        if (fat_mount(&volume, ata_read_sectors, 0)) {
+            return;
+        }
+        {
+            fat_u8_t mbr[512];
+            unsigned int index;
+
+            if (!ata_read_sectors(0, 1, mbr) || mbr[510] != 0x55U || mbr[511] != 0xAAU) {
+                return;
+            }
+            for (index = 0; index < 4U; index++) {
+                const fat_u8_t *partition = mbr + 446U + (index * 16U);
+                fat_u8_t type = partition[4];
+                fat_u32_t start = read_u32(partition, 8);
+                fat_u32_t length = read_u32(partition, 12);
+
+                if ((type == 0x04U || type == 0x06U || type == 0x0EU) && length != 0U &&
+                    start < ata_sector_count() && length <= ata_sector_count() - start &&
+                    fat_mount(&volume, ata_read_sectors, start)) {
+                    return;
+                }
+            }
+        }
     }
 }
 
